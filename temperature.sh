cfdbad22beaaa8bde7af0de00852f9c474cf1d7e
chaselynd <chaselynd@ksu.edu>
2018-09-07 16:30:46 -0500

    Completed bash script to convert Fahrenheit to Celsius.

---
+++ temperature.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script takes a single argument (a temperature in Fahrenheit) and ouputs
+# the equivalent temperature in Celcius.
+
+# assign the passed in argument to the variable tempF
+tempF=$1
+
+# convert from F to C
+tempC=$(echo "scale=4;($tempF-32)*(5/9)" | bc)
+
+# output the temperature conversion to the terminal
+echo "$tempF F = $tempC C"
