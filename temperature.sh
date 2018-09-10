8136e52a4f073b639ce02e4fab13977900ab60a5
Alec Mitchell <acmitch@ksu.edu>
2018-09-10 12:48:30 -0500

    Modify temperature.sh to include Kelvin

--- temperature.sh
+++ temperature.sh
@@ -8,6 +8,7 @@ tempF=$1
 
 # convert from F to C
 tempC=$(echo "scale=4;($tempF-32)*(5/9)" | bc)
+tempK=$(echo "scale=4;($tempC+273)" | bc)
 
 # output the temperature conversion to the terminal
-echo "$tempF F = $tempC C"
+echo "$tempF F = $tempC C = $tempK K"
